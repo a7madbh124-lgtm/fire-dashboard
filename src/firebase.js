087fab104ed3ba7fc313a28a29b283cfb51cba8b
a7madbh124-lgtm <a7madbh124@gmail.com>
2025-12-26 23:34:49 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,22 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyCSGlNROS6fv-Cv_jDUMwFu_hB47BNfdq8",
-    authDomain: "fire-detection-system-65059.firebaseapp.com",
-    databaseURL: "https://fire-detection-system-65059-default-rtdb.firebaseio.com",
-    projectId: "fire-detection-system-65059",
-    storageBucket: "fire-detection-system-65059.firebasestorage.app",
-    messagingSenderId: "182876633372",
-    appId: "1:182876633372:web:62996e33fece8ed737e2d9",
-    measurementId: "G-QSE3LFQ6N2"
+
+    apiKey: "AIzaSyBQW1809f2PicabNaCoqLGPpLqg9b0aAPA",
+
+    authDomain: "fire-detection-system-34815.firebaseapp.com",
+
+    databaseURL: "https://fire-detection-system-34815-default-rtdb.firebaseio.com",
+
+    projectId: "fire-detection-system-34815",
+
+    storageBucket: "fire-detection-system-34815.firebasestorage.app",
+
+    messagingSenderId: "425432533026",
+
+    appId: "1:425432533026:web:6184ad06f874fb7ecc227b"
+
 };
 
 // Initialize Firebase
